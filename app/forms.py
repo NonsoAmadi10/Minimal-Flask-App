57e1f3c197b8c061644a40670bcc57697f22ca63
NonsoAmadi10 <nonsoamadi@aol.com>
2020-03-19 17:51:38 +0100

    User Login

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    name = StringField('fullname', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request, url_for
 from .forms import LoginForm
 from app import app
-
+from flask_login import current_user, login_user, logout_user, login_required
+from .models import User
+from werkzeug.urls import url_parse
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {"name": "justice"}
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,13 +18,28 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title="Home", user=user, posts=posts)
+    return render_template('index.html', title="Home", posts=posts)
 
 
 @app.route('/login', methods =['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'login requested for user {form.name.data}, remember_me={form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first() #get the user's username by filtering. Collect first result
+        if user is None or not user.check_password(form.password.data): #if the user does not exist or password is not correct
+            flash('Invalid username and password')
+            return redirect(url_for("index"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect('/index')
     return render_template('login.html', title='login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))

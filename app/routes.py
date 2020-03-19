257d3680ab1d593140b8c5f02fec09a2d652ef5d
NonsoAmadi10 <nonsoamadi@aol.com>
2020-03-19 18:11:47 +0100

    feature: user should be able to signup

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, request, url_for
-from .forms import LoginForm
-from app import app
+from .forms import LoginForm, RegistrationForm
+from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from .models import User
 from werkzeug.urls import url_parse
@@ -19,6 +19,21 @@ def index():
         }
     ]
     return render_template('index.html', title="Home", posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form) 
 
 
 @app.route('/login', methods =['GET', 'POST'])

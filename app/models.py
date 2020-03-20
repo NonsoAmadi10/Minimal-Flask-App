0b31f6ec7c2cca13a1588e3a0d19bd7a4e8638c9
NonsoAmadi10 <nonsoamadi@aol.com>
2020-03-20 14:05:45 +0100

    profile page and avatars

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -6,6 +7,8 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname= db.Column(db.String(120))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -19,6 +22,9 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
     
 @login.user_loader
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 from flask import render_template, flash, redirect, request, url_for
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, EditProfileForm
 from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from .models import User
+from datetime import datetime
 from werkzeug.urls import url_parse
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -53,6 +62,41 @@ def login():
         return redirect('/index')
     return render_template('login.html', title='login', form=form)
 
+
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts= [
+        {
+            'author': user,
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': user,
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+     
+    return render_template('user.html', posts=posts, user=user, title= f'{user.username} profile page')
+    
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        current_user.fullname = form.fullname.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+        form.fullname.data = current_user.fullname
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 @app.route('/logout')
 def logout():

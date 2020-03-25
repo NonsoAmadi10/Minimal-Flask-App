2a5630c32e3d858519527faa2870ed88dc01a741
Houdini <nonsoamadi@aol.com>
2020-03-25 15:33:20 +0100

    finish pagination

--- app/config.py
+++ app/config.py
@@ -6,3 +6,10 @@ class Config(object):
     SECRET_KEY = environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = environ.get('DB_URL') or f'sqlite:///{path.join(basedir, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = environ.get('MAIL_SERVER')
+    MAIL_PORT = int(environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    ADMINS = [environ.get('ADMIN')]
+    POSTS_PER_PAGE = 1
--- app/models.py
+++ app/models.py
@@ -4,6 +4,23 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100))
+    body = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname= db.Column(db.String(120))
@@ -13,6 +30,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -25,6 +47,24 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    def follow(self, user):
+            if not self.is_following(user):
+                self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() >0 
+    
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
     
     
 @login.user_loader
@@ -33,11 +73,3 @@ def load_user(id):
         
         
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)

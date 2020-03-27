7e01a26e44f307757bac01b1d0fc7cfc06c7ce9e
Houdini <nonsoamadi@aol.com>
2020-03-27 15:33:52 +0100

    add moment to handle timezone problems

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from os import path, mkdir
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 
@@ -16,7 +18,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 mail = Mail(app)
+moment = Moment(app)
 login.login_view = 'login'
 from app import routes, errors, models
 from app.models import User, Post
